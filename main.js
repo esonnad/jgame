79587d82d2b48a28c5d5870e9bc86b381c0aa547
Emina Sonnad <esonnad@gmail.com>
2018-10-17 18:09:04 +0200

    click events

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Hello")
+$("[data-page]").hide();
+$("[data-page='home']").show();
+
+function goToPage(link) {
+  $("[data-page]").hide();
+  $("[data-page=" + link + "]").show();
+}
+
+$("a").click(function(event) {
+  event.preventDefault();
+  var href = $(this).attr('href');
+  goToPage(href);
+
+})
+
+//hide all the dibs with data-page, except the right one
+
